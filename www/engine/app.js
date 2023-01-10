86a2991c27e4418e56fc3596e9d7f02d3b3b8216
Abraham <brhmdeen@gmail.com>
2023-01-10 05:07:25 +0100

    updated

--- www/engine/app.js
+++ www/engine/app.js
@@ -1,7 +1,7 @@
 var app=angular.module('obicwallet', ['ionic','chart.js','ionic-segment','ionic.native','monospaced.qrcode', 'ngCordova', 'ngStorage','ngFileUpload'])
 .constant('Config', {  
-    // API:'https://obictrade.com/api/',
-    API:'http://localhost:7000/',
+    API:'https://obictrade.com/api/',
+    // API:'http://localhost:7000/',
     media:'https://storage.googleapis.com/obicstorage/',
     token:"eyJhbGciOiJIUzI1NiJ9.eyJTZWNyZXQiOiJ0cmFkZW9iaWNAZ21haWwuY29tIiwiSXNzdWVyIjoiSXNzdWVyIiwiZXhwIjoxNjUxODUyMjU4LCJpYXQiOjE2NTE4NTIyNTh9.WsvtTMfX4KQJW3wveHc7q5khQQtVCqxQpjJFzjBjR6A"
   })
